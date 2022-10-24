7035e654c2d4aed32300586bb155d4b178cc3af4
daniel iabanji <daniel.iabanji@yahoo.com>
2022-10-24 16:50:51 +0200

    finish con bous 1-2-3

--- main.js
+++ main.js
@@ -1,3 +1,24 @@
+"use strict";
+
+// Milestone 1 - Creiamo il nostro array di oggetti che rappresentano ciascun post.
+// Ogni post dovrà avere le informazioni necessarie per stampare la relativa card:
+// - id del post, numero progressivo da 1 a n
+// - nome autore,
+// - foto autore,
+// - data in formato americano (mm-gg-yyyy),
+// - testo del post,
+// - immagine (non tutti i post devono avere una immagine),
+// - numero di likes.
+// *Non è necessario creare date casuali*
+// *Per le immagini va bene utilizzare qualsiasi servizio di placeholder ad es. Unsplash (https://unsplash.it/300/300?image=<id>)*
+// Milestone 2 - Prendendo come riferimento il layout di esempio presente nell'html, stampiamo i post del nostro feed.
+// Milestone 3- Se clicchiamo sul tasto "Mi Piace" cambiamo il colore al testo del bottone e incrementiamo il counter dei likes relativo.
+// Salviamo in un secondo array gli id dei post ai quali abbiamo messo il like.
+// BONUS
+//  1. Formattare le date in formato italiano (gg/mm/aaaa)
+// 2. Gestire l'assenza dell'immagine profilo con un elemento di fallback che contiene le iniziali dell'utente (es. Luca Formicola > LF).
+//  3. Al click su un pulsante "Mi Piace" di un post, se abbiamo già cliccato dobbiamo decrementare il contatore e cambiare il colore del bottone.
+
 const posts = [
     {
         "id": 1,
@@ -55,3 +76,108 @@ const posts = [
         "created": "2021-03-05"
     }
 ];
+
+
+const containerHtml = document.getElementById('container');
+
+// Funzione per creare i post
+function drawPost (){
+    
+    formatITDateAll()
+    posts.forEach((value)=> {
+        const post = document.createElement('div');
+        post.className = 'post';
+        post.innerHTML= `
+        <div class="post__header">
+                <div class="post-meta">                    
+                    <div class="post-meta__icon">
+                        <img class="profile-pic" src="${value.author.image}" alt="Phil Mangione">                    
+                    </div>
+                    <div class="post-meta__data">
+                        <div class="post-meta__author">${value.author.name}</div>
+                        <div class="post-meta__time">${value.created}</div>
+                    </div>                    
+                </div>
+            </div>
+            <div class="post__text">${value.content}</div>
+            <div class="post__image">
+                <img src="${value.media}" alt="">
+            </div>
+            <div class="post__footer">
+                <div class="likes js-likes">
+                    <div class="likes__cta">
+                        <a class="like-button  js-like-button" href="#" data-postid="1">
+                            <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                            <span class="like-button__label">Mi Piace</span>
+                        </a>
+                    </div>
+                    <div class="likes__counter">
+                        Piace a <b id="like-counter-1" class="js-likes-counter">${value.likes}</b> persone
+                    </div>
+                </div> 
+            </div>            
+        `;
+        containerHtml.append(post);
+
+    })
+}
+drawPost();
+
+
+// funzione per quando non c'è img icona utente
+const postMetaIcon = document.querySelectorAll('.post-meta__icon');
+
+postMetaIcon.forEach((document, i) => {
+
+    if (posts[i].author.image === null) {
+        let c = '';
+        posts[i].author.name.split(' ').forEach(h => {
+            c += h.charAt().toUpperCase();
+        });
+        document.innerHTML = `<div class="letter-upper-case">${c}</div>`;
+    }
+})
+
+
+const likesButton = document.querySelectorAll('.likes')
+const likedPostsIDs = [];
+
+
+// funzione per mettere like
+likesButton.forEach((document)=>{
+
+    const buttonLike = document.querySelector('.like-button');
+    const jsLikeButton = document.querySelector('.js-likes-counter');
+
+    const nameIdLike = + 1;
+    likedPostsIDs.push({[nameIdLike]: false});
+
+    buttonLike.addEventListener('click', ()=> {
+        buttonLike.classList.toggle('like-button--liked');
+
+        const defaultLike = parseInt(jsLikeButton.innerHTML);
+
+        if(buttonLike.className === 'like-button js-like-button like-button--liked'){
+            jsLikeButton.innerHTML = defaultLike + 1;
+            likedPostsIDs[i][nameIdLike] = true;
+
+        }else {
+            jsLikeButton.innerHTML = defaultLike - 1;
+            likedPostsIDs[i][nameIdLike] = false;
+        }
+
+    })
+
+
+})
+
+
+// Funzione per formattare la data nel formato italiano
+function formatITDateAll() {
+    for (let i = 0; i < posts.length; i++) {
+        let dateArray = posts[i].created.split("-");
+        let newDate = dateArray[2] + "-" + dateArray[1] + "-" + dateArray[0];
+        posts[i].created = newDate;
+        console.log(newDate);
+    }
+}
